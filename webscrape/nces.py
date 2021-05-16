12c56f2994b23249b919c2a025b3de9fcf6c464f
elainespak <elainespak@gmail.com>
2021-05-16 17:19:04 +0900

    Update nces major scraping code

--- webscrape/nces.py
+++ webscrape/nces.py
@@ -53,6 +53,17 @@ key_df.to_csv('./data/nces_majors_key.csv', index=False)
 
 
 ### Major Page Example
+
+definition_list = []
+seealso_list = []
+change_list = []
+textchange_list = []
+examples_list = []
+code2020_list = []
+title2020_list = []
+code2010_list = []
+title2010_list = []
+
 url = 'https://nces.ed.gov/ipeds/cipcode/cipdetail.aspx?y=56&cipid=90509'#'https://nces.ed.gov/ipeds/cipcode/cipdetail.aspx?y=56&cipid=91089'
 n = randint(1, 10) / randint(2, 4)
 
@@ -67,26 +78,49 @@ soup = BeautifulSoup(htmlSource, 'html.parser')
 
 # Definition
 cipdetail = soup.find('div', {'class': 'cipdetail'})
-definition_p = str(cipdetail.find_all('p')[1])
-definition = re.findall('(?<=<p><strong>Definition:</strong>).*?(?=</p>)', definition_p)[0]
+definition_p = cipdetail.find_all('p')[1]
+definition = re.findall('(?<=<p><strong>Definition:</strong>).*?(?=. <)', str(definition_p))[0]
+definition = definition.strip()
+
+# See also
+seealso_span = definition_p.find('span').text
+seealso = re.findall('(?<=See also:).*', seealso_span)[0]
+seealso = seealso.strip()
 
 # Change
-change_p = str(cipdetail.find_all('p')[2])
-change = re.findall('(?<=<p><strong>Action:</strong>).*?(?=</p>)', change_p)[0]
+change_p = cipdetail.find_all('p')[2]
+change = re.findall('(?<=<p><strong>Action:</strong>).*?(?=</p>)', str(change_p))[0]
+change = change.strip()
 
 # Text Change
-textchange_p = str(cipdetail.find_all('p')[3])
-textchange = re.findall('(?<=<p><strong>Text Change: </strong>).*?(?=</p>)', textchange_p)[0]
-
-# Summary Crosswalk (2010 vs. 2020)
-summary_crosswalk = soup.find('div', {'class': 'summary_crosswalk'})
-table = summary_crosswalk.find_all('tr')[2]
-
-codes = [code.text for code in table.find_all('td', {'class':'code'})]
-titles = [title.text for title in table.find_all('td', {'class':'titledesc'})]
-#action = table.find('td', {'class':'action'}).find('img')['alt']
+textchange_p = cipdetail.find_all('p')[3]
+try:
+    textchange = re.findall('(?<=<p><strong>Text Change: </strong>).*?(?=</p>)', str(textchange_p))[0]
+    textchange = textchange.strip()
+except:
+    textchange = 'None available'
 
 # Examples
 summary_examples = soup.find('div', {'class': 'summary_examples'})
 codesummary = summary_examples.find('ul', {'class': 'codesummary'})
-examples = [li.text for li in codesummary.find_all('li')]
+examples = ':::::'.join([li.text.strip() for li in codesummary.find_all('li')])
+
+# Summary Crosswalk (2010 vs. 2020)
+summary_crosswalk = soup.find('div', {'class': 'summary_crosswalk'})
+table = summary_crosswalk.find_all('tr')[2]
+codes = [code.text.strip() for code in table.find_all('td', {'class': 'code'})]
+titles = [title.text.strip() for title in table.find_all('td', {'class': 'titledesc'})]
+
+
+# All
+definition_list.append(definition)
+seealso_list.append(seealso)
+change_list.append(change)
+textchange_list.append(textchange)
+examples_list.append(examples)
+code2020_list.append(codes[1])
+title2020_list.append(titles[1])
+code2010_list.append(codes[0])
+title2010_list.append(titles[0])
+
+
