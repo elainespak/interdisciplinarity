665f5498ea659dacdf8fae551b2f5cd6cad47895
elainespak <elainespak@gmail.com>
2021-05-28 13:18:50 +0900

    Move STEM merge code to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -42,3 +42,48 @@ def preprocess(document):
         preprocessed_text = ' '.join(tokens)
 
         return preprocessed_text
+
+### TODO: Delete later
+### Add STEM category to NCES Majors
+import re
+import pandas as pd
+
+
+def replace(x):
+    try:
+        if not pd.isnull(x):
+            x = re.sub('\s+', ' ', x)
+    except:
+        print(f'{x} caused problems')
+    return x
+
+all_df = pd.read_csv('data/nces_majors_all.csv')
+all_df['category'] = all_df['category'].apply(lambda x: replace(x))
+all_df.to_csv('data/nces_majors_all.csv')
+
+
+stem = pd.read_csv('data/nces_majors_stem.csv')
+
+def reformat(c):
+    c = c[3:]
+    if 'Moved from' in c:
+        c = re.match('.*?(?= Moved from )', c).group(0)
+    if '. New' in c:
+        c = re.match('.*?(?=. New)', c).group(0) + '.'
+    if not c.endswith('.'):
+        c += '.'
+    new_c = c[:7] + ')' + c[7:]
+    return new_c
+
+stem['category'] = stem['category'].apply(lambda x: reformat(x))
+stem['stem'] = 'y'
+
+all_stem_df = all_df.merge(stem, on='category', how='left')
+test = all_df.merge(stem, on='category', how='right')
+test = test[pd.isnull(test['definition'])]
+
+# Check
+len(stem) == len(all_stem_df[all_stem_df['stem']=='y']) + len(test)
+
+all_stem_df['stem'] = all_stem_df['stem'].apply(lambda x: 'n' if pd.isnull(x) else 'y')
+all_stem_df.to_csv('data/nces_majors_all_stem.csv')
